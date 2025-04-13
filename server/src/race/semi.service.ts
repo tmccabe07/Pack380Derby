53af59ec326a62ee839e619c9f5f1a100429050c
tmccabe07 <tmccabe07@users.noreply.github.com>
2025-04-13 11:43:21 -0400

    semi update
    updated logic to create semis after deadheats are done and fully ready to advance.  However, the shuffle doesn't seem to work properly because the semis are all the same lane assignments

--- server/src/race/semi.service.ts
+++ server/src/race/semi.service.ts
@@ -4,11 +4,30 @@ import { Injectable } from '@nestjs/common';
 export class SemiGlobalVariableService {
   private numAdvances: number  = 0;
 
+  private advanceToSemis = [
+    { 
+      carId: 0,
+      aggResults: 0, 
+    },
+  ];
+
   getNumAdvances(): number {
     return this.numAdvances;
   }
 
   setnumAdvances(value: number): void {
     this.numAdvances = value;
+  }
+
+  getAdvanceToSemis() {
+    return this.advanceToSemis;
+  }
+
+  setAdvanceToSemis(data){    
+    this.advanceToSemis = data;
+  }
+
+  initAdvanceToSemis(){
+    this.advanceToSemis.length = 0;
   }
 }

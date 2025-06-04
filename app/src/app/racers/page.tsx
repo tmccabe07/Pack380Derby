260371f1b81352db6e014fc4e862648307483833
Andrew Turner <ajturner@highearthorbit.com>
2025-06-03 20:54:39 -0400

    Added create for racers and cars

--- app/src/app/racers/page.tsx
+++ app/src/app/racers/page.tsx
@@ -9,7 +9,7 @@ export default function CarsPage() {
   return (
     <Layout>
       <h1 className="text-3xl font-bold mb-8">All Cars</h1>
-      <Link href="/racers/create" className="bg-green-600 text-white px-6 py-3 rounded hover:bg-green-700 mb-6 inline-block">
+      <Link href="/racers/create" className="bg-green-600 text-white px-6 py-3 rounded hover:bg-green-700 mb-6 inline-block" aria-label="Create Racer">
         Register New Racer
       </Link>
 

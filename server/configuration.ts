ec24588f77299fc9ff868f0219d01bb9901c3979
tmccabe07 <Tanu.d.mccabe@gmail.com>
2026-01-04 14:19:54 -0500

    add support for ssl
    add support for ssl.  Env variable USE_SSL="false" or "true".  Expects derby.crt and derby.key to exist in server/

--- server/configuration.ts
+++ server/configuration.ts
@@ -1,5 +1,6 @@
 export const configuration = () => ({
   NODE_ENV: process.env.NODE_ENV,
   port: 3000,
-  database_url: process.env.DATABASE_URL
+  database_url: process.env.DATABASE_URL,
+  use_ssl: process.env.USE_SSL === 'true'
 });
